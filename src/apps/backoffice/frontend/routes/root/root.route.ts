5e1dae68792d24b0864ad7a5565f3fe9e29abb8b
AnderGI <agibarguen@gmail.com>
2025-02-15 13:34:04 +0100

    Setup config

--- src/apps/backoffice/frontend/routes/root/root.route.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, Router } from 'express';
-import path from 'node:path';
-
-export const register = (router: Router): void => {
-	router.get('/', (req: Request, res: Response) => {
-		res.sendFile(path.join(__dirname, '..', '..', '/public/login.html'));
-	});
-};
