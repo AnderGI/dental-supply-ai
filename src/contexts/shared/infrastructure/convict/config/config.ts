44716806bada81b01b74db0fc659a342cc8fcdc1
AnderGI <agibarguen@gmail.com>
2025-02-15 09:55:16 +0100

    Removed .env. N¡Dont have to worry about secret variables for now

--- src/contexts/shared/infrastructure/convict/config/config.ts
+++ src/contexts/shared/infrastructure/convict/config/config.ts
@@ -10,38 +10,38 @@ const config = convict({
 		doc: 'The application environment.',
 		format: ['production', 'dev', 'test'],
 		env: 'NODE_ENV',
-		default: process.env.NODE_ENV
+		default: 'dev'
 	},
 	typeorm: {
 		host: {
 			doc: 'The database host',
 			format: String,
 			env: 'TYPEORM_HOST',
-			default: process.env.TYPEORM_HOST
+			default: 'localhost'
 		},
 		port: {
 			doc: 'The database port',
 			format: Number,
 			env: 'TYPEORM_PORT',
-			default: process.env.TYPEORM_PORT
+			default: 5432
 		},
 		username: {
 			doc: 'The database username',
 			format: String,
 			env: 'TYPEORM_USERNAME',
-			default: process.env.TYPEORM_USERNAME
+			default: 'admin'
 		},
 		password: {
 			doc: 'The database password',
 			format: String,
 			env: 'TYPEORM_PASSWORD',
-			default: process.env.TYPEORM_PASSWORD
+			default: 'p@ssw0rd'
 		},
 		database: {
 			doc: 'The database name',
 			format: String,
 			env: 'TYPEORM_DATABASE',
-			default: process.env.TYPEORM_DATABASE
+			default: 'backoffice-backend-dev'
 		}
 	},
 	rabbitmq: {
@@ -50,37 +50,37 @@ const config = convict({
 				doc: 'RabbitMQ communication protocol',
 				format: String,
 				env: 'RABBITMQ_PROTOCOL',
-				default: process.env.RABBITMQ_PROTOCOL
+				default: 'amqp'
 			},
 			hostname: {
 				doc: 'RabbitMQ hostname',
 				format: String,
 				env: 'RABBITMQ_HOSTNAME',
-				default: process.env.RABBITMQ_HOSTNAME
+				default: 'localhost'
 			},
 			port: {
 				doc: 'RabbitMQ communication port',
 				format: Number,
 				env: 'RABBITMQ_PORT',
-				default: process.env.RABBITMQ_PORT
+				default: '5672'
 			},
 			username: {
 				doc: 'RabbitMQ username',
 				format: String,
 				env: 'RABBITMQ_USERNAME',
-				default: process.env.RABBITMQ_USERNAME
+				default: 'admin'
 			},
 			password: {
 				doc: 'RabbitMQ password',
 				format: String,
 				env: 'RABBITMQ_PASSWORD',
-				default: process.env.RABBITMQ_PASSWORD
+				default: 'p@ssw0rd'
 			},
 			vhost: {
 				doc: 'RabbitMQ vhost',
 				format: String,
 				env: 'RABBITMQ_VHOST',
-				default: process.env.RABBITMQ_VHOST
+				default: '/'
 			}
 		},
 		publishOptions: {
@@ -88,20 +88,20 @@ const config = convict({
 				doc: 'Content type for published messages',
 				format: String,
 				env: 'RABBITMQ_PUBLISH_CONTENT_TYPE',
-				default: process.env.RABBITMQ_PUBLISH_CONTENT_TYPE
+				default: 'application/json'
 			},
 			contentEncoding: {
 				doc: 'Content encoding for published messages',
 				format: String,
 				env: 'RABBITMQ_PUBLISH_CONTENT_ENCODING',
-				default: process.env.RABBITMQ_PUBLISH_CONTENT_ENCODING
+				default: 'utf-8'
 			}
 		}
 	}
 });
 
 // Load environment-dependent configuration
-const env = config.get('env') as string;
+const env = config.get('env');
 
 config.loadFile(path.join(__dirname, '/', `convict_${env}.json`));
 
